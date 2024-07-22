e5dec822c20ca8ee0e81304ff6112fcf5df34106
Sanjay Yadav <sanjay8874002308@gmail.com>
2024-07-22 13:44:32 +0530

    Added the properties to connect application to database.

---
+++ Reward-program-Project/src/main/java/rewardProgram/com/RewardProgramProjectApplication.java
@@ -0,0 +1,13 @@
+package rewardProgram.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardProgramProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardProgramProjectApplication.class, args);
+	}
+
+}
---
+++ Reward-program-Project/src/test/java/rewardProgram/com/RewardProgramProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package rewardProgram.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardProgramProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
